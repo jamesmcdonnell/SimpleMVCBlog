80d1b5f3af4c727175d278ff0feef6ece7605735
jamesmcdonnell <james.mcdonnell@fleetmatics.com>
2015-03-09 17:23:47 +0000

    Added Basic Login Form

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web.WebPages;
+using SimpleBlog.ViewModels;
 using System.Web.Mvc;
 
 namespace SimpleBlog.Controllers
@@ -10,8 +8,26 @@ namespace SimpleBlog.Controllers
     {
         public ActionResult Login()
         {
-            return Content("Login!");
+            return View(new AuthLogin
+            {
+             //Test = "This is my test value set in my control"  
+            });
         }
 
+       [HttpPost]
+        public ActionResult Login(AuthLogin form)
+       {
+           if (!ModelState.IsValid)
+           {
+               return View(form);
+           }// form.Test = "This is a value set in my post action";
+
+           if (form.Username != "correct name")
+           {
+               ModelState.AddModelError("Username", "Username not quite right!");
+               return View(form);
+           }
+           return Content("The form is Valid!");
+       }
     }
 }
