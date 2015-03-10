c2ed6de9a04ababd785dc8d26e9eb51e3fe3d6f3
jamesmcdonnell <james.mcdonnell@fleetmatics.com>
2015-03-10 15:06:08 +0000

    Imported styles and scripts (JQuery and Bootstrap)
    Set up admin and normal user Script and Style bundles. Altered bootstrap
    to be able to find the fonts in the '~content/fonts' folder

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SimpleBlog
+{
+    public class BundleConfig
+    {
+        //Need a style and a script bundle for both front end and backend
+        public static void RegisterBundles(BundleCollection bundles )
+        {
+                                            // Style Bundles
+
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
+
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/site.css"));
+
+                                            //Script Bundles
+            // ensure to call in this order because of dependencies
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-2.1.3.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootsrap.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-2.1.3.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootsrap.js"));
+        }
+    }
+}
